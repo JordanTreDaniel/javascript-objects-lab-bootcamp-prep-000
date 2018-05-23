31751f5c28ee14848d3011e961bfdbd83f004479
christleyjordan@gmail.com Christley <christleyjordan@gmail.com>
2018-05-23 23:15:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, val) {
-  var clone = Object.assign({}, obj, {key: val});
+  var clone = Object.assign({}, obj, {[key]: val});
   return clone;
 }
